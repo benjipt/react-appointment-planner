a0eaf279a3045ca977ce450852d1676579da0364
[Benji Thompson] <[thompson.benji@gmail.com]>
2021-03-15 20:32:12 -0400

    8.ok

--- src/components/appointmentForm/AppointmentForm.js
+++ src/components/appointmentForm/AppointmentForm.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { ContactPicker } from '../contactPicker/ContactPicker'
 
 export const AppointmentForm = ({
   contacts,
@@ -20,6 +21,32 @@ export const AppointmentForm = ({
   };
 
   return (
-    AppointmentForm
+    <form onSubmit={handleSubmit}>
+      <input 
+        type='text'
+        value={title}
+        placeholder='Meeting Title'
+        onChange={({target}) => {setTitle(target.value)}}
+        required
+      />
+      <input 
+        type='date'
+        value={date}
+        onChange={({target}) => {setDate(target.value)}}
+        min={getTodayString()}
+        required
+      />
+      <input 
+        type='time'
+        value={time}
+        onChange={({target}) => {setTime(target.value)}}
+      />
+      <ContactPicker 
+        contacts={contacts}
+        value={contact}
+        onChange={({target}) => setContact(target.value)}
+      />
+      <input type='submit' />
+    </form>
   );
 };

cda2f62ae20c6d2abc42d94d732dd8bcd03f70e2
[Benji Thompson] <[thompson.benji@gmail.com]>
2021-03-15 20:51:16 -0400

    finished project

--- src/components/contactPicker/ContactPicker.js
+++ src/components/contactPicker/ContactPicker.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-export const ContactPicker = () => {
+export const ContactPicker = ({contacts, onChange}) => {
   return (
-    ContactPicker
+    <select onChange={onChange}>
+      <option
+        value=''
+        key='0'
+        selected='selected'>Select Contact</option>
+      {contacts.map( contact => <option value={contact.name} key={contact.name}>{contact.name}</option>)}
+    </select>
   );
 };

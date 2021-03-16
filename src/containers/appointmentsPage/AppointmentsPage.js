10fef07af192dd8f75a3e0a4fb3e8e1f2a858ece
[Benji Thompson] <[thompson.benji@gmail.com]>
2021-03-15 20:15:23 -0400

    7.ok

--- src/containers/appointmentsPage/AppointmentsPage.js
+++ src/containers/appointmentsPage/AppointmentsPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import { AppointmentForm } from '../../components/appointmentForm/AppointmentForm'
+import { TileList } from '../../components/tileList/TileList'
 
 export const AppointmentsPage = (props) => {
   // Define state variables for appointment info
-  const contacts = props.contacts;
   const appointments = props.appointments;
+  const contacts = props.contacts;
   const addAppointment = props.addAppointment;
   // local state variables
   const [title, setTitle] = useState('');
@@ -14,17 +16,37 @@ export const AppointmentsPage = (props) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     // Add contact info and clear data  
-   
+    addAppointment(title, contact, date, time);
+    // reset values after submit
+    setTitle('');
+    setContact('');
+    setDate('');
+    setTime('');
   };
 
   return (
     <div>
       <section>
         <h2>Add Appointment</h2>
+        <AppointmentForm 
+          title={title}
+          contact={contact}
+          date={date}
+          time={time}
+          setTitle={setTitle}
+          setContact={setContact}
+          setDate={setDate}
+          setTime={setTime}
+          handleSubmit={handleSubmit}
+          contacts={contacts}
+        />
       </section>
       <hr />
       <section>
         <h2>Appointments</h2>
+        <TileList 
+          objArr={appointments}
+        />
       </section>
     </div>
   );
